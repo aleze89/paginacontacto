91a29bfa1c0a94ee4e0b2e69bcfd87cf4a7100d2
gabialemoya <gmgabiale72@gmail.com>
2020-10-26 19:36:24 -0300

    Creacion de pagina Contacto y toma de datos con POST

---
+++ Grupo5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Grupo5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
